b6790ee7b72bdea8874003bcdef90d96c19c6611
Robert Hemfelt <rhemfelt@gmail.com>
2017-02-17 15:56:31 -0600

    Create story.js

---
+++ story.js
@@ -0,0 +1,15 @@
+var name = prompt("Hello dipshit! Give me any name: ");
+var vomit = prompt("Give me a household appliance: ");
+var verb = prompt("Give me a past tense verb: ");
+var body = prompt("Give me a body part: ");
+var color = prompt("Give me a color: ");
+
+
+alert('Thanks for the story! Here it is: ');
+document.write('There once was a Prince named ' + name); 
+document.write('. He got ever so sick and threw up in the ' + vomit);
+document.write('. Embarassed, he hastily ' + verb);
+document.write(' the vomit with his ' + body);
+document.write(', but his ' + body);
+document.write(' turned ' + color);
+document.write(' and he died.');
