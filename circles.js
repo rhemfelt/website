3713a94d8c69b6ddb5e382fcdfbdb5165324ce6d
Robert Fuckface <rhemfelt@gmail.com>
2017-03-24 15:10:06 -0500

    100 circles

--- circles.js
+++ circles.js
@@ -5,7 +5,7 @@ var blue;
 var rgbColor;
 i = 0;
 
-for (i = 1; i <= 5000; i+=1) {
+for (var i = 0; i < 100; i+=1) {
   red = Math.floor(Math.random() * 256 );
 green = Math.floor(Math.random() * 256 );
 blue = Math.floor(Math.random() * 256 );
