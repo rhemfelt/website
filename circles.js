242497886cc4e26520d854aaf9fd456f13156dfa
Robert Fuckface <rhemfelt@gmail.com>
2017-03-24 15:04:54 -0500

    5000 circles

--- circles.js
+++ circles.js
@@ -5,13 +5,11 @@ var blue;
 var rgbColor;
 i = 0;
 
-for (i  = 1; i <= 500; i += 1) {
+for (i = 1; i <= 5000; i+=1) {
   red = Math.floor(Math.random() * 256 );
 green = Math.floor(Math.random() * 256 );
 blue = Math.floor(Math.random() * 256 );
 rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
-html += '<div style="background-color:' + rgbColor + '"></div>'; 
-  }
-
-
+html += '<div style="background-color:' + rgbColor + '"></div>';
+}
 document.write(html);
