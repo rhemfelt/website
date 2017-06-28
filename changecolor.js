73611acccadc1c9afbe14b9beed9ae8b45f44706
Robert Hemfelt <rhemfelt@gmail.com>
2017-06-28 15:23:50 -0500

    add button below change color text

--- changecolor.js
+++ changecolor.js
@@ -1,5 +1,6 @@
 const myHeading = document.getElementById('myHeading');
+const myButton = document.getElementById('myButton');
 
-myHeading.addEventListener('click', () => {
+myButton.addEventListener('click', () => {
 	myHeading.style.color = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
 });
