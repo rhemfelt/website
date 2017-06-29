c4aad96afb97c412ebb3f8a06042b34f76aaa2c5
Robert Hemfelt <rhemfelt@gmail.com>
2017-06-29 13:35:47 -0500

    update click me button to show gif on click

--- changecolor.js
+++ changecolor.js
@@ -3,4 +3,8 @@ const myButton = document.getElementById('myButton');
 
 myButton.addEventListener('click', () => {
 	myHeading.style.color = '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+	var sourceOfPicture = "http://www.reactiongifs.com/r/2013/10/tim-and-eric-mind-blown.gif";
+  var img = document.getElementById('bigpic')
+  img.src = sourceOfPicture.replace('180x180', '450x450');
+  img.style.display = "block";
 });
