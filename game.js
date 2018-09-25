4f10988356664ae6e16d53d768f86592a77b56fc
Robert Gray Hemfelt <rhemfelt@MacBook-Pro-4.local>
2018-09-25 17:05:42 -0500

    add shrekspin

--- game.js
+++ game.js
@@ -87,7 +87,7 @@ window.onload = function () {
     
     //Load spritesheets
     game.load.spritesheet('player', 'shrek.png', 50, 61);
-    game.load.spritesheet('coin', 'coin.png', 36, 44);
+    game.load.spritesheet('coin', 'shrekspin.png', 50, 38);
     game.load.spritesheet('badge', 'badge.png', 42, 54);
 
   }
