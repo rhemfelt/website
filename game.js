815d00d09af2721d6aa56f07d4554312c0cb50a7
Robert Gray Hemfelt <rhemfelt@MacBook-Pro-4.local>
2018-10-02 11:28:50 -0500

    shrek 30 start

--- game.js
+++ game.js
@@ -141,7 +141,7 @@ window.onload = function () {
 
   // initial game set up
   function create() {
-    player = game.add.sprite(31, 550, 'player');
+    player = game.add.sprite(30, 550, 'player');
     player.animations.add('walk');
     player.anchor.setTo(0.5, 1);
     game.physics.arcade.enable(player);
