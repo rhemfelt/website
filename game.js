344025c3149c05243c862094845de7eab4079503
Robert Gray Hemfelt <rhemfelt@MacBook-Pro-4.local>
2018-10-02 11:15:37 -0500

    move shrek up

--- game.js
+++ game.js
@@ -115,7 +115,7 @@ window.onload = function () {
 
   // initial game set up
   function create() {
-    player = game.add.sprite(100, 600, 'player');
+    player = game.add.sprite(50, 550, 'player');
     player.animations.add('walk');
     player.anchor.setTo(0.5, 1);
     game.physics.arcade.enable(player);
