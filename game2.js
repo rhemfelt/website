2e99d87451ccb45de516ed834d0316e499775dbf
Robert Gray Hemfelt <rhemfelt@Roberts-MacBook-Pro.local>
2018-10-20 16:44:24 -0500

    move farquaads

--- game2.js
+++ game2.js
@@ -35,8 +35,8 @@ function addItems() {
   createItem(230, 15, 'star');
   createItem(760, 5, 'star');
   createItem(150, 210, 'farquaad');
-  createItem(740, 260, 'farquaad');
-  createItem(330, 565, 'farquaad');
+  createItem(735, 260, 'farquaad');
+  createItem(330, 560, 'farquaad');
 }
 
 // add platforms to the game
