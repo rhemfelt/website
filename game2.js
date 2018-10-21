84eb36f1c50236e567bfdec757918ea929dc7834
Robert Gray Hemfelt <rhemfelt@Roberts-MacBook-Pro.local>
2018-10-20 20:52:04 -0500

    sdf

--- game2.js
+++ game2.js
@@ -31,7 +31,7 @@ function addItems() {
   createItem(375, 100, 'onion');
   createItem(370, 500, 'onion');
   createItem(100, 375, 'onion');
-  createItem(150, 200, 'onion');
+  createItem(220, 235, 'onion');
   createItem(710, 5, 'onion');
   createItem(230, 15, 'star');
   createItem(760, 5, 'star');
