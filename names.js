b6dfb549a2438436e4bb69ec864831ed05c6df0d
Robert Hemfelt <rhemfelt@gmail.com>
2020-07-12 18:34:14 -0500

    add kasey and allyn

--- names.js
+++ names.js
@@ -1,4 +1,4 @@
-var names = ["Dustin","Robert","Taylor","Maddi","Andrea","Jen","Zac","Meranda","Maggi","Henry", "Lauren", "Devlin"];
+var names = ["Dustin","Robert","Taylor","Maddi","Andrea","Jen","Zac","Meranda","Maggi","Henry", "Lauren", "Devlin", "Kasey", "Allyn"];
 
 if (names.length % 2 != 0) {
     alert("You must have an even number of names. You currently have " + names.length + " names.");
